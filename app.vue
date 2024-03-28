2c413e40bcaea8d52bc84d491a2356374cbe34ce
gruguy <gruguy@163.com>
2024-03-28 23:37:43 +0800

    init nuxt project

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <RouterView />
+  </div>
+</template>
+<script setup lang="ts">
+useHead({
+  title: "我的应用",
+  meta: [{ name: "description", content: "我的神奇网站ffffff。" }],
+  bodyAttrs: {
+    class: "test",
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+useSeoMeta({
+  title: "我的神奇网站",
+  ogTitle: "我的神奇网站",
+  description: "这是我的神奇网站，让我来告诉你关于它的一切。",
+  ogDescription: "这是我的神奇网站，让我来告诉你关于它的一切。",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
+});
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: {
+      level: 2,
+    },
+    config: {},
+    viewer: true,
+  },
+});
